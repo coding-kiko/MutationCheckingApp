5a26f1f05c28cf372643c52351137e7cdd8769f3
coding-kiko <2789@holbertonschool.com>
2022-07-29 13:12:48 -0300

    validating dna is square matrix

--- pkg/mutant/service.go
+++ pkg/mutant/service.go
@@ -1,6 +1,8 @@
 package mutant
 
 import (
+	"fmt"
+
 	"github.com/coding-kiko/MutantCheckingApp/pkg/errors"
 )
 
@@ -29,14 +31,34 @@ func (m *mutantService) GetStats() (Stats, error) {
 
 func (m *mutantService) AnalyzeDna(req DnaRequest) error {
 	sample := NewDnaSample(req.Dna)
+
+	if sample.Size < 4 {
+		return errors.NewBadRequest("DNA sample not long enough")
+	}
+	err := VerifySquareMatrix(sample.Dna)
+	if err != nil {
+		return err
+	}
+
 	sample.CheckMutation()
 
-	err := m.lab.StoreDna(*sample)
+	err = m.lab.StoreDna(*sample)
 	if err != nil {
 		return err
 	}
 	if !sample.Mutant {
 		return errors.NewNotMutant()
+	}
+	return nil
+}
+
+// Received and array of strings and returns an error if it is not NxN
+func VerifySquareMatrix(matrix []string) error {
+	rows := len(matrix)
+	for i, row := range matrix {
+		if len(row) != rows {
+			return errors.NewBadRequest(fmt.Sprintf("DNA not represented by a square matrix: element %d", i+1))
+		}
 	}
 	return nil
 }
